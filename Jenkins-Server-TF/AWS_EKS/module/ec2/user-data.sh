56bc7915458811aff802581bfc81a70fc8657f5e
venkatesh-reddy679 <60383183+venkatesh-reddy679@users.noreply.github.com>
2024-09-17 13:29:10 -0400

    Update user-data.sh

--- Jenkins-Server-TF/AWS_EKS/module/ec2/user-data.sh
+++ Jenkins-Server-TF/AWS_EKS/module/ec2/user-data.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+#installing SSM
+sudo apt update
+curl https://s3.amazonaws.com/amazon-ssm-us-east-1/latest/debian_amd64/amazon-ssm-agent.deb -o amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl start amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
 
 #installing AWS cli
 sudo apt update
