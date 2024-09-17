cb2751e0cd4f4a31a05f82bec297e2b13b5f8df3
venkatesh-reddy679 <60383183+venkatesh-reddy679@users.noreply.github.com>
2024-09-17 14:47:30 -0400

    Update user-data.sh

--- Jenkins-Server-TF/AWS_EKS/module/ec2/user-data.sh
+++ Jenkins-Server-TF/AWS_EKS/module/ec2/user-data.sh
@@ -2,6 +2,7 @@
 #installing AWS cli
 sudo apt update
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
 
