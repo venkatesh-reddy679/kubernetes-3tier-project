2b1ed4cc9713662b3b7db0ab57bd5fc2b80e3aaa
venkatesh-reddy679 <60383183+venkatesh-reddy679@users.noreply.github.com>
2024-09-17 14:58:02 -0400

    Update user-data.sh

--- Jenkins-Server-TF/AWS_EKS/module/ec2/user-data.sh
+++ Jenkins-Server-TF/AWS_EKS/module/ec2/user-data.sh
@@ -18,7 +18,7 @@ sudo apt-get install -y kubectl
 
 #installing eksctl
 sudo apt update
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
