5ebbae26964ad76538e952f6f718d5e9fa4b3edc
venkatesh-reddy679 <60383183+venkatesh-reddy679@users.noreply.github.com>
2024-09-17 14:20:08 -0400

    Update main.tf

--- Jenkins-Server-TF/AWS_EKS/dev/main.tf
+++ Jenkins-Server-TF/AWS_EKS/dev/main.tf
@@ -39,8 +39,7 @@ module "jump_server" {
   instance_type          = var.jump_server_instance_type
   ami                    = var.jump_server_ami
   name                   = var.jump_server_name
-  subnet_id              = module.vpc.vpc_public_subnets[0]
-  vpc_security_group_ids = [module.vpc.vpc_default_security_group]
+  subnet_id              = module.vpc.vpc_private_subnets[0]
   vpc_id                     = module.vpc.vpc_id
 }
 
